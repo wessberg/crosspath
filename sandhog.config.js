590a711f283887294710bf90516badbd3b3f6d96
wessberg <frederikwessberg@gmail.com>
2021-06-03 13:52:10 +0200

    chore(build): update dependencies

--- sandhog.config.js
+++ sandhog.config.js
@@ -1,7 +1,15 @@
-module.exports = {
-	...require("@wessberg/ts-config/sandhog.config"),
+/* eslint-disable @typescript-eslint/no-require-imports */
+// @ts-check
+
+/**
+ * @type {import("helpertypes").PartialDeep<import("sandhog").SandhogConfig>}
+ */
+const config = {
+	...require("@wessberg/ts-config/sandhog.config.json"),
 	logo: {
 		url: "https://raw.githubusercontent.com/wessberg/crosspath/master/documentation/asset/logo.png",
 		height: 100
 	}
 };
+
+module.exports = config;
